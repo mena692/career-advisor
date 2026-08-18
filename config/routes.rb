cd3630195a4c84dfe22a88379971b37307aab7eb
Marvin Mena <mena0692@gmail.com>
2026-08-18 15:07:52 +0200

    added first controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :career_paths, only: [:index, :show] do
+    resources :chats, only: [:create]
+  end
+
+  resources :chats, only: [:show] do
+    resources :messages, only: [:create]
+  end
+
 end
